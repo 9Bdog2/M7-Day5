8431e9e3277facc1467a326b07cc02becaec5caf
Bogdan Birau <birau.bogdan92@gmail.com>
2022-01-28 11:31:41 +0100

    setting up second project

--- spotify-react/src/components/AlbumCard.jsx
+++ spotify-react/src/components/AlbumCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const AlbumCard = ({ song }) => (
   <div className="col text-center" id={song.id}>
-    {console.log(song.id)}
+    {console.log(song)}
     <Link to={"/album/" + song.album.id}>
       <div>
         <img className="img-fluid" src={song.album.cover_medium} alt="1" />
